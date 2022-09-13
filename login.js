175a0a48b2eac0ac2428805f4802387aeabc7199
Ayushi Soni <soniayushi345@gmail.com>
2022-09-13 22:54:29 +0530

    again commiting the file as it wasn't added to the repository

--- login.js
+++ login.js
@@ -1,3 +1,5 @@
+//problem1:check prime
+
 let arr=[1,2,3,4,5]
 
 for(let i=1;i<=arr.length-1;i++){
@@ -8,3 +10,18 @@ else{
 console.log("No);
 }
 }
+
+//problem2: check palindrome
+
+str2[];
+for(let i=N-1.i>=0;i--){
+str2.push(str[i])
+}
+if(str==str2.join(''){
+console.log("Yes");
+}
+else{
+console.log("No)
+}
+}
+
