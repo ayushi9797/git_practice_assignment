97bf177e66e1fb5be98deca846655f13e27bc536
Prabhat Shukla <prabhatshukla9797@gmail.com>
2022-09-13 23:43:10 +0530

    login

--- login.js
+++ login.js
@@ -3,11 +3,11 @@
 let arr=[1,2,3,4,5]
 
 for(let i=1;i<=arr.length-1;i++){
-if(i%1!==0 && i%i!=0){
-console.log("Yes);
+if(i%1==0 && i%i==0){
+console.log("Yes");
 }
 else{
-console.log("No);
+console.log("No");
 }
 }
 
@@ -21,7 +21,7 @@ if(str==str2.join(''){
 console.log("Yes");
 }
 else{
-console.log("No)
+console.log("No")
 }
 }
 
